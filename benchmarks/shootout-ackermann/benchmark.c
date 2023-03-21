19bc6a85e14043aeb1caf905eaced6cb53cf8e60
Johnnie Birch <johnnie.l.birch.jr@intel.com>
2023-03-20 18:30:08 -0700

    cleanup forgotten rebase update needed for ackermann

--- benchmarks/shootout-ackermann/benchmark.c
+++ benchmarks/shootout-ackermann/benchmark.c
@@ -25,21 +25,12 @@ int read_int_from_file(char *path)
     int fd = open(path, 0);
     assert(fd != -1);
 
-<<<<<<< HEAD:benchmarks/shootout-ackermann/benchmark.c
-    ssize_t m =0, n = 0;
+    ssize_t m = 0, n = 0;
     do {
         m = read(fd, (void*) &buf, sizeof(buf) - n - 1);
         assert(m >= 0);
         n += m;
     } while (m > 0);
-=======
-    ssize_t n = 0;
-    do
-    {
-        n += read(fd, (void *)&buf, sizeof(buf) - n - 1);
-        assert(n >= 0);
-    } while (n > 0);
->>>>>>> 65cd73d (Add support for running all of shootout natively):benchmarks-next/shootout-ackermann/benchmark.c
     assert(close(fd) == 0);
 
     buf[n] = '\0';
